44065ff710f55873c8bc652477dd1b14d7294f03
zwemansoekun <45864942+zwemansoekun@users.noreply.github.com>
2024-02-05 17:35:47 +0630

    final push 5/2/2024

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
-
+$(document).ready(function() {   
     $('#get_resume').click(function(e) {
-        e.preventDefault();  //stop the browser from following
-        window.location.href = 'upload/THIDA_HTAY_履　歴　書.xls';
-    });
-    
+        e.preventDefault();  //stop the browser from following          
+        window.location.href = 'upload/TDH_Japan_CV&Resume.zip';      
+    });    
 });
