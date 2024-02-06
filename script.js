223163167e8899b2803bc44837784a29d691b1f2
zwemansoekun <45864942+zwemansoekun@users.noreply.github.com>
2024-02-06 11:44:29 +0630

    updated eng & jp cv

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {   
-    $('#get_resume').click(function(e) {
+    $('#get_jpcv').click(function(e) {
         e.preventDefault();  //stop the browser from following          
-        window.location.href = 'upload/TDH_Japan_CV&Resume.zip';      
-    });    
+        window.location.href = 'upload/JP_CV_FORM.zip';      
+    });   
+    
+    $('#get_engcv').click(function(e) {
+        e.preventDefault();  //stop the browser from following          
+        window.location.href = 'upload/THIDA_HTAY_CV.docx';      
+    });  
 });
